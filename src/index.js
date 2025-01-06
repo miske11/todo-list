52c70ee7bc156f09d9f678c6ea6e8e07d4ba8682
Luka Misic <lukamisic2001@gmail.com>
2025-01-06 22:32:27 +0100

    Added basic display of list items

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-import Item from "./item"
-import Project from "./project.js"
+import Item from "./item";
+import Project from "./project.js";
+import "./style.css";
+import { ScreenControler } from "./screen.js";
 
 const item1 = new Item('test', 'testing item', '5.1.2025', 'low');
-item1.check();
-console.log(item1);
+const item2 = new Item('test2', 'testing item2', '5.1.2025', 'low');
+const item3 = new Item('test3', 'testing item3', '5.1.2025', 'high');
 
 const project = new Project('my project', 'this is just a test project');
 project.addItem(item1);
-project.addItem(item1);
-project.addItem(item1);
-project.addItem(item1);
-project.removeItem(2)
+project.addItem(item2);
+project.addItem(item3);
 
-console.log(project);
+const sc = new ScreenControler();
+sc.drawProject(project);
+
+const addItemBtn = document.querySelector('.addItem');
+addItemBtn.addEventListener('click', () => {
+    const content = document.querySelector('.conent');
+    content.textContent = '';
+})
---
+++ src/screen.js
@@ -0,0 +1,22 @@
+import Project from "./project"
+import Item from "./item"
+export const ScreenControler =  class {
+    
+    drawItem(item) {
+        const content = document.querySelector('.content');
+        const itemContainer = document.createElement('div');
+        itemContainer.classList.add('item-container');
+        content.appendChild(itemContainer);
+        for (const value of Object.values(item)) {
+            const field = document.createElement('p');
+            field.textContent = value;
+            itemContainer.appendChild(field);
+        }
+    }
+
+    drawProject(project) {
+        for (const item of project.items) {
+            this.drawItem(item);
+        }
+    }
+}
